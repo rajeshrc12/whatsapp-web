37de6156de5db9f0f955c8017b3b908349f150dc
rajeshrc12 <rajesh.charhajari@gmail.com>
2024-03-20 18:35:17 +0530

    new ui

--- client-vite/src/App.jsx
+++ client-vite/src/App.jsx
@@ -1,39 +1,98 @@
-import React, { useCallback } from "react";
-import LeftPanel from "./components/leftpanel/LeftPanel";
-import RightPanel from "./components/rightpanel/RightPanel";
-import MiddlePanel from "./components/middlepanel/MiddlePanel";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import data from "./data/data";
+
 const App = () => {
-  const mainValue = useSelector((state) => state.panel.main);
-  const render = useCallback(() => {
-    switch (mainValue) {
-      case "status":
-        return <div className="fixed h-screen w-screen bg-white">Status</div>;
-      case "showRightPanel":
-        return (
-          <>
-            <div className="border col-span-3">
-              <MiddlePanel />
-            </div>
-            <div className="border col-span-2 overflow-y-scroll">
-              <RightPanel />
-            </div>
-          </>
-        );
-      default:
-        return (
-          <div className="border col-span-5">
-            <MiddlePanel />
-          </div>
-        );
-    }
-  }, [mainValue]);
+  const [rightPanel, setRightPanel] = useState(false);
+  const [document, setDocument] = useState(false);
   return (
-    <div className="grid grid-cols-7 h-screen w-screen">
-      <div className="border col-span-2 overflow-hidden">
-        <LeftPanel />
+    <div className="grid grid-cols-12 h-screen w-screen">
+      <div className="col-span-3 border">
+        <div className="grid grid-rows-12 h-full">
+          <div className="row-span-1 border">1</div>
+          <div className="row-span-1 border">2</div>
+          <div className="row-span-10 border relative overflow-y-scroll">
+            <div className="absolute top-0 left-0 h-full">
+              {new Array(30).fill(0).map((d) => (
+                <div>hello</div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-      {render()}
+      <div className={`col-span-${rightPanel ? 6 : 9} border`}>
+        <div className="grid grid-rows-12 h-full">
+          <div className="row-span-1 border">
+            <button onClick={() => setRightPanel(!rightPanel)}>
+              right panel
+            </button>
+            <button onClick={() => setDocument(!document)}>document</button>
+          </div>
+          {document ? (
+            <>
+              <div className="row-span-11 border">
+                <div className="grid grid-rows-12 h-full">
+                  <div className="row-span-1 border">1</div>
+                  <div className="row-span-8 border">2</div>
+                  <div className="row-span-1 border bg-gray-300">
+                    <div className="flex justify-center items-center h-full">
+                      <input
+                        type="text"
+                        className="w-2/3 outline-none p-2 rounded-lg"
+                      />
+                    </div>
+                  </div>
+                  <div className="row-span-3 border">
+                    <div className="grid grid-cols-12 h-full">
+                      <div className="col-span-11 border relative overflow-x-scroll">
+                        <div className="absolute flex top-0 left-0 h-full w-full">
+                          <div
+                            style={{
+                              display: "grid",
+                              gridTemplateColumns: "100px",
+                              gridAutoFlow: "column",
+                            }}
+                          >
+                            {new Array(30).fill(0).map((d) => (
+                              <div>hello</div>
+                            ))}
+                            {/* <div className="sticky bg-white top-0 right-0 border"></div> */}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-span-1 border">2</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="row-span-10 border relative overflow-y-scroll">
+                <div className="absolute top-0 left-0 h-full">
+                  {new Array(30).fill(0).map((d) => (
+                    <div>hello</div>
+                  ))}
+                </div>
+              </div>
+              <div className="row-span-1 border">3</div>
+            </>
+          )}
+        </div>
+      </div>
+      {rightPanel && (
+        <div className="col-span-3 border">
+          <div className="grid grid-rows-12 h-full">
+            <div className="row-span-1 border">1</div>
+            <div className="row-span-11 border relative overflow-y-scroll">
+              <div className="absolute top-0 left-0 h-full">
+                {new Array(30).fill(0).map((d) => (
+                  <div>hello</div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

bb0af5418ef6241d172bf9d069619b4612176f22
rajeshrc12 <rajesh.charhajari@gmail.com>
2024-04-23 14:42:44 +0530

    env server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const { handleSocket } = require("./controllers/socket-controller");
 const app = express();
 const port = 3001;
 require("dotenv").config();
-app.use(cors({ origin: "*", methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 app.use(bodyParser.json());
 
 Connection(
